d11fa10a418ca02b794d32c162da525d56a6e190
AbdalrahmanAlzaro <abdalrahman.h.alzaro@gmail.com>
2023-03-19 14:28:41 +0300

    edit

--- app.js
+++ app.js
@@ -1,19 +1,42 @@
+// if statement
+// alert("Welcome to Caffe-Z")
+
+// let name = prompt("Please enter your name:");
+// let gender = prompt("Please enter your gender (male or female):");
+
+// if (gender == "male") {
+//   alert("Welcome, Mr " + name + "!");
+// } else if (gender == "female") {
+//   alert("Welcome, Ms " + name + "!");
+// } else {
+//   alert("Welcome!");
+// }
+
+// let drinkType = prompt("Would you like a hot or cold drink?");
+// let drinkName = prompt("What kind of drink would you like?");
+
+// alert("Your " + drinkType + " " + drinkName + " is being prepared!");
+
+// console.log(name + " ordered a " + drinkType + " " + drinkName + ".");
+
+
+
+
+//for loops 
 alert("Welcome to Caffe-Z")
+let answers = [];
+let username = prompt("What is your name?");
+let gender = "";
 
-let name = prompt("Please enter your name:");
-let gender = prompt("Please enter your gender (male or female):");
-
-if (gender == "male") {
-  alert("Welcome, Mr " + name + "!");
-} else if (gender == "female") {
-  alert("Welcome, Ms " + name + "!");
-} else {
-  alert("Welcome!");
+while (gender !== "male" && gender !== "female") {
+  gender = prompt("What is your gender? (male/female)");
 }
 
 let drinkType = prompt("Would you like a hot or cold drink?");
 let drinkName = prompt("What kind of drink would you like?");
 
-alert("Your " + drinkType + " " + drinkName + " is being prepared!");
+answers.push(username, gender, drinkType, drinkName);
 
-console.log(name + " ordered a " + drinkType + " " + drinkName + ".");
+for (let i = 0; i < answers.length; i++) {
+  console.log(answers[i]);
+}
